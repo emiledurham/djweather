16c7cecbaae77dcd9c86db3a351aa029b82f82ae
Emile Durham <durhamemile@gmail.com>
2021-03-15 13:35:07 -0700

    intitial commit

---
+++ lookup/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about')
+]
+
+# note to self:
+# {{ '/' }} in html links to home or {% url 'home' %}
+# {{ 'about' }} in html links to home or {% url 'about' %}
+# ! USE url syntax to pull name because path could change
+
