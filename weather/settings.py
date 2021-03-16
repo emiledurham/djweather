971cd6d90a89913d0a37650d502f9e7fe819d3ca
Emile Durham <durhamemile@gmail.com>
2021-03-15 17:46:30 -0700

    security improvements

--- weather/settings.py
+++ weather/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&7b2tdds^gb$d*xzboaon4)&96l(40st%65$t%n(bk!9k^1@_('
+SECRET_KEY = os.environ.get('DJWEATHER_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
